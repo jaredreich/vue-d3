fa459d14536f17f6b018f2fcd6e102116ef6b84b
Trevor <trevorhreed@users.noreply.github.com>
2018-02-28 15:27:56 -0700

    updating browser reference to d3

--- vue-d3.js
+++ vue-d3.js
@@ -2,7 +2,7 @@
   var VueD3 = {};
   var D3 = typeof require === 'function'
     ? require('d3')
-    : window.D3;
+    : window.d3;
 
   if (!D3) {
     throw new Error('[vue-d3] cannot locate d3');
